bf11ad9f035d07b5ec63ffdccf71c723820009fa
Jérémie Langlade <jlanglade@pixbuf.net>
2015-04-13 14:03:48 +0200

    Add google ads (disabled for now)

--- themes/default/footer.php
+++ themes/default/footer.php
@@ -1,4 +1,18 @@
 		</div>
+<?php
+/*
+		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+		<!-- WoT Clan Tool -->
+		<ins class="adsbygoogle"
+			 style="display:block"
+			 data-ad-client="ca-pub-2557330068328405"
+			 data-ad-slot="5125006902"
+			 data-ad-format="auto"></ins>
+		<script>
+		(adsbygoogle = window.adsbygoogle || []).push({});
+		</script>
+*/
+?>
 		<div id="footer">
 			<div id="footerPayPalDonate">
 				<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
