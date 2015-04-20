b9a1095632ebbf7e0cd63ab4b3cab492c42b9117
Jérémie Langlade <jlanglade@pixbuf.net>
2015-04-20 16:54:55 +0200

    Add Google adSense advertising.

--- index.php
+++ index.php
@@ -66,6 +66,16 @@ if (isset($_SESSION["access_token"])) {
 		<div class="container-fluid">
 			<div class="row">
 				<div class="main">
+					<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+					<!-- WoT Clan Tool -->
+					<ins class="adsbygoogle"
+						 style="display:block"
+						 data-ad-client="ca-pub-2557330068328405"
+						 data-ad-slot="5125006902"
+						 data-ad-format="auto"></ins>
+					<script>
+					(adsbygoogle = window.adsbygoogle || []).push({});
+					</script>
 					<h1 data-i18n="app.name"></h1>
 					<p data-i18n="[html]page.index.lines.0"></p>
 					<p data-i18n="[html]page.index.lines.1"></p>
--- unauthorized.php
+++ unauthorized.php
@@ -39,6 +39,16 @@ require(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'server' . DIRECTORY_SEPARATOR
 		<div class="container-fluid">
 			<div class="row">
 				<div class="main">
+					<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+					<!-- WoT Clan Tool -->
+					<ins class="adsbygoogle"
+						 style="display:block"
+						 data-ad-client="ca-pub-2557330068328405"
+						 data-ad-slot="5125006902"
+						 data-ad-format="auto"></ins>
+					<script>
+					(adsbygoogle = window.adsbygoogle || []).push({});
+					</script>
 					<h1 data-i18n="page.unauthorized.title"></h1>
 					<p data-i18n="page.unauthorized.lines.0"></p>
 					<p data-i18n="page.unauthorized.lines.1"></p>
